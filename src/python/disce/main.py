bc9c80ea488b3ee812f55d53969127319ea62bd9
Julian Valentin <19839841+valentjn@users.noreply.github.com>
2025-09-07 07:51:40 +0200

    Set up logging

--- src/python/disce/main.py
+++ src/python/disce/main.py
@@ -6,12 +6,21 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """Main module."""
 
-from disce.screens import edit_decks as edit_decks_screen  # noqa: F401
+import logging
+
+# import all screens to register event handlers
+from disce.screens import *  # noqa: F403  # isort: skip
+
+from disce import tools
 from disce.screens import load as load_screen
 from disce.screens import main as main_screen
+
+_logger = logging.getLogger(__name__)
 
 
 def main() -> None:
     """Run the main application logic."""
+    tools.set_up_logging()
+    _logger.info("application started")
     main_screen.show()
     load_screen.hide()

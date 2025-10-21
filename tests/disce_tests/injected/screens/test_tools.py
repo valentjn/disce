984551a65958831ebec40eaf4359261f7d3c6103
Julian Valentin <19839841+valentjn@users.noreply.github.com>
2025-10-21 17:44:29 +0200

    Add some tests for screens.tools

---
+++ tests/disce_tests/injected/screens/test_tools.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# Copyright (C) 2025 Julian Valentin
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from collections.abc import Generator
+
+import pytest
+from disce.screens.tools import Element, Event, append_child, create_element, hide_element, set_theme, show_element
+from pyscript import document, window
+
+
+class TestCreateElement:
+    @staticmethod
+    def test_tag() -> None:
+        element = create_element("div")
+        assert element.tagName == "DIV"
+
+    @staticmethod
+    def test_children() -> None:
+        child1 = create_element("span")
+        child2 = create_element("p")
+        parent = create_element("div", child1, child2)
+        assert parent.childNodes.length == 2
+        assert parent.childNodes[0].isSameNode(child1)
+        assert parent.childNodes[1].isSameNode(child2)
+
+    @staticmethod
+    def test_text() -> None:
+        element = create_element("div", text="test")
+        assert element.innerText == "test"
+
+    @staticmethod
+    def test_html() -> None:
+        element = create_element("div", html="<span>inner</span>")
+        assert element.innerHTML == "<span>inner</span>"
+
+    @staticmethod
+    def test_event_listeners() -> None:
+        triggered = {"click": False, "mouseover": False}
+
+        def on_click(event: Event) -> None:
+            triggered["click"] = True
+
+        def on_mouseover(event: Event) -> None:
+            triggered["mouseover"] = True
+
+        element = create_element(
+            "div",
+            event_listeners={
+                "click": on_click,
+                "mouseover": on_mouseover,
+            },
+        )
+        element.dispatchEvent(window.Event.new("click"))
+        assert triggered["click"]
+        element.dispatchEvent(window.Event.new("mouseover"))
+        assert triggered["mouseover"]
+
+    @staticmethod
+    def test_attributes() -> None:
+        element = create_element("div", id_="test_id", data_value="test_value")
+        assert element.getAttribute("id") == "test_id"
+        assert element.getAttribute("data-value") == "test_value"
+
+
+def test_append_child() -> None:
+    parent = create_element("div")
+    child = append_child(parent, "span", text="child")
+    assert parent.childNodes.length == 1
+    assert parent.childNodes[0].isSameNode(child)
+    assert child.tagName == "SPAN"
+    assert child.innerText == "child"
+
+
+@pytest.fixture
+def inserted_element() -> Generator[Element]:
+    element = create_element("div")
+    document.body.appendChild(element)
+    yield element
+    document.body.removeChild(element)
+
+
+def test_hide_element(inserted_element: Element) -> None:
+    style = window.getComputedStyle(inserted_element)
+    hide_element(inserted_element)
+    assert style.display == "none"
+    hide_element(inserted_element, hide=False)
+    assert style.display != "none"
+
+
+def test_show_element(inserted_element: Element) -> None:
+    style = window.getComputedStyle(inserted_element)
+    show_element(inserted_element)
+    assert style.display != "none"
+    show_element(inserted_element, show=False)
+    assert style.display == "none"
+
+
+@pytest.fixture
+def restore_theme() -> Generator[None]:
+    original_theme = document.documentElement.getAttribute("data-bs-theme")
+    yield
+    if original_theme is None:
+        document.documentElement.removeAttribute("data-bs-theme")
+    else:
+        document.documentElement.setAttribute("data-bs-theme", original_theme)
+
+
+@pytest.mark.parametrize("theme", ["dark", "light"])
+def test_set_theme(theme: str, restore_theme: None) -> None:
+    set_theme(theme)
+    assert document.documentElement.getAttribute("data-bs-theme") == theme
+
+
+def test_set_theme_auto(restore_theme: None) -> None:
+    set_theme()
+    assert document.documentElement.getAttribute("data-bs-theme") in {"dark", "light"}

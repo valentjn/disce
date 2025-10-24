2331076867732754c01896a61d179fa88879b2e6
Julian Valentin <19839841+valentjn@users.noreply.github.com>
2025-10-24 10:18:30 +0200

    Reduce the chance of output race conditions

---
+++ tests/disce_tests/injected/conftest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Copyright (C) 2025 Julian Valentin
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from time import sleep
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def wait() -> None:
+    # sleep a bit before every test to reduce the chance of output being written by the injected tests while the output
+    # capture is disabled by the calling test
+    sleep(0.1)

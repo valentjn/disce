93656de948ada09d3fb60a38e1450cda182fa6fa
Julian Valentin <19839841+valentjn@users.noreply.github.com>
2025-10-25 20:12:11 +0200

    Refactor deck_data_list, deck_metadata_list

--- tests/disce_tests/injected/screens/conftest.py
+++ tests/disce_tests/injected/screens/conftest.py
@@ -8,19 +8,30 @@
 from collections.abc import Generator
 
 import pytest
-from disce.data import Configuration
+from disce.data import Configuration, DeckData, DeckMetadata, UUIDModelList
 from disce.storage.local import LocalStorage
 
 from disce_tests.injected.screens.tools import create_decks
 
 
+@pytest.fixture
+def deck_data_list() -> list[DeckData]:
+    deck_data_list, _ = create_decks("")
+    return deck_data_list.root
+
+
+@pytest.fixture
+def deck_metadata_list() -> list[DeckMetadata]:
+    _, deck_metadata_list = create_decks("")
+    return deck_metadata_list.root
+
+
 @pytest.fixture(autouse=True)
-def decks_and_configuration() -> Generator[None]:
+def decks_and_configuration(deck_data_list: list[DeckData], deck_metadata_list: list[DeckMetadata]) -> Generator[None]:
     local_storage = LocalStorage()
-    deck_data_list, deck_metadata_list = create_decks("")
     for deck_data in deck_data_list:
         deck_data.save_to_storage(local_storage)
-    configuration = Configuration(deck_metadata=deck_metadata_list, history_length=2)
+    configuration = Configuration(deck_metadata=UUIDModelList(deck_metadata_list), history_length=2)
     configuration.save_to_storage(local_storage)
     yield
     local_storage.clear()

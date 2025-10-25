c0a79a4690549852ecbb8233e4dc9af5f0e6fcb4
Julian Valentin <19839841+valentjn@users.noreply.github.com>
2025-10-25 21:20:34 +0200

    Add tests for add_deck and import_decks

--- tests/disce_tests/injected/screens/test_decks.py
+++ tests/disce_tests/injected/screens/test_decks.py
@@ -6,10 +6,16 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from collections.abc import Sequence
+from unittest.mock import call, patch
 
 import pytest
+from disce.data import DeckData, DeckExport, DeckMetadata, ExportedDeck
 from disce.screens.decks import DecksScreen
+from disce.screens.edit_deck import EditDeckScreen
 from disce.storage.local import LocalStorage
+
+from disce_tests.injected.screens.tools import assert_decks, create_decks
+from disce_tests.injected.tools import assert_hidden, assert_visible
 
 
 class TestDecksScreen:
@@ -41,3 +47,106 @@ class TestDecksScreen:
         rows = screen.select_child(".disce-decks").children
         assert len(rows) == 1
         assert rows[0].innerText == "No decks available. Please add a deck."
+
+    @staticmethod
+    def test_add_deck(screen: DecksScreen) -> None:
+        screen.add_deck()
+        assert_hidden(screen)
+        assert_visible(EditDeckScreen(None, LocalStorage()))
+
+    @staticmethod
+    @pytest.fixture
+    def deck_export() -> DeckExport:
+        deck_data_list, deck_metadata_list = create_decks("exported")
+        return DeckExport(
+            decks=[
+                ExportedDeck(data=deck_data, metadata=deck_metadata)
+                for deck_data, deck_metadata in zip(deck_data_list, deck_metadata_list, strict=True)
+            ]
+        )
+
+    @staticmethod
+    def test_import_decks(
+        deck_data_list: list[DeckData],
+        deck_metadata_list: list[DeckMetadata],
+        screen: DecksScreen,
+        deck_export: DeckExport,
+    ) -> None:
+        screen.import_decks()
+        with patch(
+            "disce.screens.decks.upload_file", side_effect=lambda _, listener: listener(deck_export.model_dump_json())
+        ):
+            screen.import_decks()
+        TestDecksScreen._assert_rendered_decks(
+            screen, ["deck1_name", "deck2_name", "exported_deck1_name", "exported_deck2_name"]
+        )
+        assert_decks(
+            deck_data_list + [deck.data for deck in deck_export.decks],
+            deck_metadata_list + [deck.metadata for deck in deck_export.decks],
+        )
+
+    @staticmethod
+    def test_import_decks_invalid_json(screen: DecksScreen) -> None:
+        with (
+            patch(
+                "disce.screens.decks.upload_file",
+                side_effect=lambda _, listener: listener("invalid_json"),
+            ),
+            patch("disce.screens.decks.alert") as alert_mock,
+        ):
+            screen.import_decks()
+        assert alert_mock.call_count == 1
+        assert (
+            alert_mock.call_args_list[0]
+            .args[0]
+            .startswith("Failed to parse imported data: 1 validation error for DeckExport\n")
+        )
+
+    @staticmethod
+    def test_import_decks_overwrite_approved(
+        deck_data_list: list[DeckData],
+        deck_metadata_list: list[DeckMetadata],
+        screen: DecksScreen,
+        deck_export: DeckExport,
+    ) -> None:
+        deck_export.decks[0].data.uuid = "deck1"
+        deck_export.decks[0].metadata.uuid = "deck1"
+        with (
+            patch(
+                "disce.screens.decks.upload_file",
+                side_effect=lambda _, listener: listener(deck_export.model_dump_json()),
+            ),
+            patch("disce.screens.decks.confirm", return_value=True) as confirm_mock,
+        ):
+            screen.import_decks()
+        assert confirm_mock.call_args_list == [
+            call(
+                "The imported data contains 1 deck (see below) that will overwrite existing decks. Do you want to "
+                'continue?\n\nName of deck to be overwritten: "deck1_name"'
+            )
+        ]
+        TestDecksScreen._assert_rendered_decks(screen, ["deck2_name", "exported_deck1_name", "exported_deck2_name"])
+        assert_decks(
+            deck_data_list[1:] + [deck.data for deck in deck_export.decks],
+            deck_metadata_list[1:] + [deck.metadata for deck in deck_export.decks],
+        )
+
+    @staticmethod
+    def test_import_decks_overwrite_declined(
+        deck_data_list: list[DeckData],
+        deck_metadata_list: list[DeckMetadata],
+        screen: DecksScreen,
+        deck_export: DeckExport,
+    ) -> None:
+        deck_export.decks[0].data.uuid = "deck1"
+        deck_export.decks[0].metadata.uuid = "deck1"
+        with (
+            patch(
+                "disce.screens.decks.upload_file",
+                side_effect=lambda _, listener: listener(deck_export.model_dump_json()),
+            ),
+            patch("disce.screens.decks.confirm", return_value=False),
+        ):
+            screen.import_decks()
+        TestDecksScreen._assert_rendered_decks(screen, ["deck1_name", "deck2_name"])
+        assert_decks(deck_data_list, deck_metadata_list)
--- tests/disce_tests/injected/screens/tools.py
+++ tests/disce_tests/injected/screens/tools.py
@@ -6,7 +6,10 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 
-from disce.data import Card, DeckData, DeckMetadata, UUIDModelList
+from collections.abc import Sequence
+
+from disce.data import Card, Configuration, DeckData, DeckMetadata, UUIDModel, UUIDModelList
+from disce.storage.local import LocalStorage
 
 
 def create_decks(prefix: str) -> tuple[UUIDModelList[DeckData], UUIDModelList[DeckMetadata]]:
@@ -37,3 +40,32 @@ def create_decks(prefix: str) -> tuple[UUIDModelList[DeckData], UUIDModelList[De
             DeckMetadata(uuid=f"{prefix}deck2", name=f"{prefix}deck2_name", number_of_cards=1),
         ]
     )
+
+
+def assert_decks(expected_deck_data: Sequence[DeckData], expected_deck_metadata: Sequence[DeckMetadata]) -> None:
+    local_storage = LocalStorage()
+    configuration = Configuration.load_from_storage(local_storage)
+    assert_same_elements(configuration.deck_metadata, expected_deck_metadata)
+    actual_deck_data = [
+        DeckData.load_from_storage(local_storage, deck_metadata.uuid) for deck_metadata in expected_deck_metadata
+    ]
+    assert_same_elements(actual_deck_data, expected_deck_data)
+
+
+def assert_same_elements[T: UUIDModel](
+    actual: Sequence[T] | UUIDModelList[T], expected: Sequence[T] | UUIDModelList[T]
+) -> None:
+    if isinstance(actual, UUIDModelList):
+        actual = actual.root
+    if isinstance(expected, UUIDModelList):
+        expected = expected.root
+    assert len(actual) == len(expected)
+    indices = set(range(len(expected)))
+    for actual_element in actual:
+        for idx in indices:
+            if actual_element == expected[idx]:
+                indices.remove(idx)
+                break
+        else:
+            msg = f"actual element {actual_element} not found in expected collection {expected}"
+            raise AssertionError(msg)
